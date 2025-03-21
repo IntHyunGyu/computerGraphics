29d9eb1b3650590f5c1036813c915fbb6de8a680
안현규 <inthyungyu@gmail.com>
2025-03-21 09:35:06 +0900

    0320 수업 완성 못한거 수정 (pypplot show)

--- 0320-4.py
+++ 0320-4.py
@@ -1,6 +1,13 @@
+import sys
+
 import cv2
 import matplotlib.pyplot as plt
 
-img=cv2.imread('img/soccer.jpg')
-h=cv2.calcHist([img],[2],None,[256],[0,256])
-plt.plot(h,color='r',linewidth=1)
+img = cv2.imread('img/soccer.jpg')
+if img is None:
+    sys.exit("파일을 찾을 수 없습니다.")
+
+h = cv2.calcHist([img],[2],None,[256],[0,256])
+plt.plot(h, color='r', linewidth=1)
+plt.show()
+
